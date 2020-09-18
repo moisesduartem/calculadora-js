c9de4576b5721eadd7a839ad5ff8fcca257bab41
moisesduartem <moisesduartem@gmail.com>
2020-09-18 15:46:23 +0000

    1a key nao pode ser uma operacao

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function(){
     const keys = getKeys();
-    keys.forEach((key, i) => {
-        key.addEventListener('click', e => {
-            console.log(key.getAttribute('class'));
+    let accumulator = '';
+    let operation = '';
+    keys.forEach(function(key, i) {
+        key.addEventListener('click', function(e){
+            let isAnOperation = key.getAttribute('class').match(/operation/g); 
+            operation = (isAnOperation) ? key.value : '';
+            
+            if (operation == '=') {
+                executeCalc(accumulator);
+                return accumulator = '';
+            }
+            
+            accumulator += (accumulator.length > 0 && isAnOperation || !isAnOperation) ? key.value : '';
+
+            console.log(accumulator);
+        
         });
     });
 });
@@ -13,3 +26,7 @@ document.addEventListener('DOMContentLoaded', function () {
 let getKeys = function () {
     return document.querySelectorAll('.key');
 } 
+
+let executeCalc = function (expression) {
+    console.log(eval(expression));
+}
