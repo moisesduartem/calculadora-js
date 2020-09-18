b04891fcc182c4eb15a0b71a777c1f3c90528439
moisesduartem <moisesduartem@gmail.com>
2020-09-18 14:31:45 +0000

    percorre todas as teclas

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const keys = document.querySelectorAll('.key');
+    keys.forEach((key, i) => {
+        console.log(key);
+    });
+});
